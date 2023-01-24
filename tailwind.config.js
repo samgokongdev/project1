1670f736345f3752871d68bd77a79900cdf2db38
APRISAL SYAM <815101162@intranet.pajak.go.id>
2023-01-24 16:28:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      fontFamily: {
+        cursive: ['Boogaloo'],
+        sans: ['Montserrat'],
+    },
+    },
+  },
+  plugins: [],
+}
